d753b969dcd0a701a81e4087323666db26279a44
Igwe Clinton <igweclintonmatro@gmail.com>
2024-10-27 14:59:53 +0100

    update

---
+++ components/MeetingModal.tsx
@@ -0,0 +1,43 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
+
+
+interface MeetingModalProps {
+    isOpen: boolean;
+    onCLose: () => void;
+    title: string;
+    className?: string;
+    children?: React.ReactNode;
+    handleClick?: () => void;
+    buttonText?: string;
+    image?: string;
+    buttonIcon?: string;
+}
+
+const MeetingModal = ({
+    isOpen, onCLose, title, className, children, handleClick, buttonText,image, buttonIcon}: MeetingModalProps) => {
+  return (
+    <Dialog>
+    <DialogTrigger>Open</DialogTrigger>
+    <DialogContent>
+        <DialogHeader>
+        <DialogTitle>Are you absolutely sure?</DialogTitle>
+        <DialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+        </DialogDescription>
+        </DialogHeader>
+    </DialogContent>
+    </Dialog>
+
+  )
+}
+
+export default MeetingModal
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,20 +1,58 @@
 "use client"
 
-import Image from "next/image"
+import HomeCard from "./HomeCard"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import MeetingModal from "./MeetingModal"
 
 const MeetingTypeList = () => {
-  return (
-    <section className='grid grif-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-        <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer' onClick={() => {}}>
+    const router = useRouter()
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting'>()
 
-            <div className="flex-center glassmorphism size-12 rounded-[10px]">
-                <Image src = '/icons/add-meeting.svg' alt="add-meeting" width={27} height={27}/>
-            </div>
-            <div className="flex flex-col gap-2">
-                <h1 className="text-sxl font-bold">New Meeting</h1>
-                <p className="text-2xl font-normal">Start an Instant meeting</p>
-            </div>
-        </div>
+    const createMeeting = () => {}
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+        <HomeCard
+            img = '/icons/add-meeting.svg'
+            title = 'New Meeting'
+            description = 'Start an Instant meeting'
+            handleClick = {() => setMeetingState('isInstantMeeting')}
+            className = 'bg-orange-1'
+         />
+
+        <HomeCard
+            img = '/icons/schedule.svg'
+            title = 'Schedule Meeting'
+            description = 'Plan your meeting'
+            handleClick = {() => setMeetingState('isScheduleMeeting')}
+            className = 'bg-green-1'
+         />
+
+        <HomeCard
+            img = '/icons/recordings.svg'
+            title = 'View Recordings'
+            description = 'Check out your recordings'
+            handleClick = {() =>  setMeetingState('isJoiningMeeting')}
+            className = 'bg-purple-1'
+         />
+
+        <HomeCard
+            img = '/icons/join-meeting.svg'
+            title = 'Join Meeting'
+            description = 'Via Invitation link'
+            handleClick = {() => setMeetingState('isJoiningMeeting')}
+            className = 'bg-yellow-1'
+         />
+
+         <MeetingModal
+         isOpen = {meetingState === 'isInstantMeeting'}
+         onCLose = {() => setMeetingState(undefined)}
+         className = 'text-center'
+         buttonText = 'Start Meeting'
+         handleClick = {createMeeting}
+         
+         />
     </section>
   )
 }
