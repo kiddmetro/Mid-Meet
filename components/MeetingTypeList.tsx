20c9e2e53c9f3ce66256295619a08fa5887dc9b1
Igwe Clinton <igweclintonmatro@gmail.com>
2024-10-31 17:43:34 +0100

    update

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,13 +4,52 @@ import HomeCard from "./HomeCard"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import MeetingModal from "./MeetingModal"
+import { useUser } from "@clerk/nextjs"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
 
 const MeetingTypeList = () => {
     const router = useRouter()
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting'>()
 
-    const createMeeting = () => {
+    const { user } = useUser()
+    const client = useStreamVideoClient();
+    const [values, setValues] = useState({
+      dateTime: new Date(),
+      description: '',
+      link: ''
+    })
 
+    const [callDetails, setCallDetails] = useState<Call>()
+  
+
+    const createMeeting = async () => {
+      if(!client || !user) return;
+
+      try{
+        const id = crypto.randomUUID();
+        const call = client.call('default', id)
+
+        if(!call) throw new Error('Call not found')
+
+        const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString()
+
+        const desription = values.description || 'Instant meeting';
+
+        await call.getOrCreate({
+          data: {
+            starts_at: startsAt,
+            custom: {
+              desription
+            }
+          }
+        })
+
+        setCallDetails(call)
+
+        if(!)
+      }catch(error){
+        console.log(error)
+      }
     }
 
   return (
